b927549769f58737ba54d6504d2cbed1fd32cc20
rmogusu <rmogusu123@gmail>
2020-05-01 17:30:18 +0200

    Instantiate endangere animal as an intance of endangered.

--- src/test/java/EndangeredTest.java
+++ src/test/java/EndangeredTest.java
@@ -6,6 +6,11 @@ import static org.junit.Assert.*;
 public class EndangeredTest {
     @Rule
     public DatabaseRule database = new DatabaseRule();
+    @Test
+    public void EndangeredAnimal_instantiatesCorrectly_true() {
+        Endangered testEndangered = new Endangered("Lion", 1);
+        assertEquals(true, testEndangered instanceof Endangered);
+    }
 
     @Test
     public void EndangeredAnimal_instantiatesWithName_String() {
