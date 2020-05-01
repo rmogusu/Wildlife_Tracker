6a84c9a6a3cc608f563776b1e357d3b6ca3c53f5
rmogusu <rmogusu123@gmail>
2020-05-01 17:28:00 +0200

    Write test to instantiate animal with its name.

--- src/test/java/EndangeredTest.java
+++ src/test/java/EndangeredTest.java
@@ -1,5 +1,16 @@
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EndangeredTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void EndangeredAnimal_instantiatesWithName_String() {
+        Endangered testEndangered = new Endangered("Lion", );
+        assertEquals("Lion", testEndangered.getName());
+    }
 
 }
