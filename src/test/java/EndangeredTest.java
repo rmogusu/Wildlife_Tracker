443bbee5b9bcaed31e1ce967f4aa8b1982a115b8
rmogusu <rmogusu123@gmail>
2020-05-01 17:41:45 +0200

    Instantiate animal with sighting id.

--- src/test/java/EndangeredTest.java
+++ src/test/java/EndangeredTest.java
@@ -17,5 +17,42 @@ public class EndangeredTest {
         Endangered testEndangered = new Endangered("Lion",1 );
         assertEquals("Lion", testEndangered.getName());
     }
+    @Test
+    public void EndangeredAnimal_instantiatesWithSightingId_int() {
+        Endangered testEndangered = new Endangered("Lion", 1);
+        assertEquals(1, testEndangered.getSightingId());
+    }
+
+//    @Test
+//    public void equals_returnsTrueIfNameAndSightingIdAreSame_true() {
+//        EndangeredAnimal testEndangeredAnimal = new EndangeredAnimal("Lion", 1);
+//        EndangeredAnimal anotherEndangeredAnimal = new EndangeredAnimal("Lion", 1);
+//        assertTrue(testEndangeredAnimal.equals(anotherEndangeredAnimal));
+//    }
+//
+//    @Test
+//    public void save_returnsTrueIfEndangeredAnimalsAreTheSame() {
+//        EndangeredAnimal testEndangeredAnimal = new EndangeredAnimal("Lion", 1);
+//        testEndangeredAnimal.save();
+//        assertTrue(EndangeredAnimal.all().get(0).equals(testEndangeredAnimal));
+//    }
+//
+//    @Test
+//    public void save_assignsIdToEndangeredAnimal() {
+//        EndangeredAnimal testEndangeredAnimal = new EndangeredAnimal("Lion", 1);
+//        testEndangeredAnimal.save();
+//        EndangeredAnimal savedEndangeredAnimal = (EndangeredAnimal) EndangeredAnimal.all().get(0);
+//        assertEquals(savedEndangeredAnimal.getId(), testEndangeredAnimal.getId());
+//    }
+//
+//    @Test
+//    public void all_returnsAllInstancesOfEndangeredAnimal_true() {
+//        EndangeredAnimal firstEndangeredAnimal = new EndangeredAnimal("Lion", 1);
+//        firstEndangeredAnimal.save();
+//        EndangeredAnimal secondEndangeredAnimal = new EndangeredAnimal("Lion", 1);
+//        secondEndangeredAnimal.save();
+//        assertEquals(true, EndangeredAnimal.all().get(0).equals(firstEndangeredAnimal));
+//        assertEquals(true, EndangeredAnimal.all().get(1).equals(secondEndangeredAnimal));
+//    }
 
 }
