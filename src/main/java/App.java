42e35b1b240045a7693454900300876d1c94dcca
rmogusu <rmogusu123@gmail>
2020-05-03 19:16:12 +0200

    Add route to my homepage.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,22 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+import static spark.Spark.*;
+import static spark.Spark.staticFileLocation;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        //get: show all Heroes in all squads and show all squads
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+//        List<Squad> squads = squadDao.getAll();
+//        model.put("squads", squads);
+//        List<Hero> heroes = heroDao.getAll();
+//        model.put("heroes", heroes);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
