70dca98825a4d347a67557f37d43e0c67b28ef2d
rmogusu <rmogusu123@gmail>
2020-05-16 19:22:46 +0200

    Add final commit

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -3,29 +3,30 @@ import org.sql2o.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 public class DB {
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-34-235-108-68.compute-1.amazonaws.com:5432/d9h45fe3o5b0fq","hgugbppvqhhfpj", "fb3a49599658e8899882d153779f9d0c9420ac8b66be90a9f2767b1bf8b10205");
 
     //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "rose", "wambua");
 
-    private static URI dbUri;
-    public static Sql2o sql2o;
-
-    static {
-
-        try {
-            if (System.getenv("DATABASE_URL") == null) {
-                dbUri = new URI("postgres://localhost:5432/wildlife_tracker");
-            } else {
-                dbUri = new URI(System.getenv("DATABASE_URL"));
-            }
-
-            int port = dbUri.getPort();
-            String host = dbUri.getHost();
-            String path = dbUri.getPath();
-            String username = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[0];
-            String password = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[1];
-
-            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
-        } catch (URISyntaxException e) {
-        }
-    }
+//    private static URI dbUri;
+//    public static Sql2o sql2o;
+//
+//    static {
+//
+//        try {
+//            if (System.getenv("DATABASE_URL") == null) {
+//                dbUri = new URI("postgres://localhost:5432/wildlife_tracker");
+//            } else {
+//                dbUri = new URI(System.getenv("DATABASE_URL"));
+//            }
+//
+//            int port = dbUri.getPort();
+//            String host = dbUri.getHost();
+//            String path = dbUri.getPath();
+//            String username = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[0];
+//            String password = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[1];
+//
+//            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+//        } catch (URISyntaxException e) {
+//        }
+//    }
 }
--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,7 +5,8 @@ import org.sql2o.Sql2o;
 public class DatabaseRule extends ExternalResource {
     @Override
     protected void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "rose", "wambua");
+        DB.sql2o = new Sql2o("jdbc:postgresql://@ec2-52-44-166-58.compute-1.amazonaws.com:5432/dck10hh0ja7lcm","dck10hh0ja7lcm", "6a200a17f78d8aba03bb4e37ecb58a1d3b9744c939bd72e96462fa940db17449");
+        //DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "rose", "wambua");
     }
 
     @Override
